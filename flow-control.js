cc854a8a800d2a0c4b14f5009f4a5d945a4abb2b
Stephanie Lella <stelella2007@gmail.com>
2019-12-29 05:44:48 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -26,8 +26,12 @@ return "You are not a teenager";}
 }
 
 function switchAge(age) {
-if (age >= 13 && age <= 19) {
-return "You are a teenager";
-} else { (age > 19 && age < 13)
-return "You have an age";}
+  switch(age){
+    case (age >= 13 && age <= 19) {
+    return "You are a teenager";
+    default:
+    return "You have an age"
+  }
 }
+
+
